9fb22acb42d7ff792e78196839dd2508e920a67a
M-Aljaafari <mujysr@gmail.com>
2025-02-15 00:35:19 +0330

    Update webpack.config.renderer.prod.babel.js

--- configs/webpack.config.renderer.prod.babel.js
+++ configs/webpack.config.renderer.prod.babel.js
@@ -1,182 +1,50 @@
 /**
- * Build config for electron renderer process
+ * Webpack config for production electron main process
  */
 
 const path = require("path");
 const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const merge = require("webpack-merge");
+const { merge } = require("webpack-merge");
 const TerserPlugin = require("terser-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const baseConfig = require("./webpack.config.base");
 const CheckNodeEnv = require("../internals/scripts/CheckNodeEnv");
 
 CheckNodeEnv("production");
-module.exports = merge.smart(baseConfig, {
+
+module.exports = merge(baseConfig, {
   devtool: "source-map",
 
   mode: "production",
 
-  target: "web",
+  target: "electron-main",
 
-  entry: path.join(__dirname, "..", "app/renderer/index"),
+  entry: "./app/main/index",
 
   output: {
-    path: path.join(__dirname, "..", "app/renderer/dist"),
-    publicPath: "./renderer/dist/",
-    filename: "renderer.prod.js",
-  },
-
-  module: {
-    rules: [
-      // Extract all .global.css to style.css as is
-      {
-        test: /\.global\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "./",
-            },
-          },
-          {
-            loader: "css-loader",
-          },
-        ],
-      },
-      // Pipe other styles through css modules and append to style.css
-      {
-        test: /^((?!\.global).)*\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "./",
-            },
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                localIdentName: "[name]__[local]__[hash:base64:5]",
-              },
-            },
-          },
-        ],
-      },
-      // Add SASS support  - compile all .global.scss files and pipe it to style.css
-      {
-        test: /\.global\.(scss|sass)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "./",
-            },
-          },
-          {
-            loader: "css-loader",
-            options: {},
-          },
-          {
-            loader: "sass-loader",
-          },
-        ],
-      },
-      // Add SASS support  - compile all other .scss files and pipe it to style.css
-      {
-        test: /^((?!\.global).)*\.(scss|sass)$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "./",
-            },
-          },
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                localIdentName: "[name]__[local]__[hash:base64:5]",
-              },
-            },
-          },
-          {
-            loader: "sass-loader",
-          },
-        ],
-      },
-      // WOFF Font
-      {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 10000,
-            mimetype: "application/font-woff",
-          },
-        },
-      },
-      // WOFF2 Font
-      {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 10000,
-            mimetype: "application/font-woff",
-          },
-        },
-      },
-      // TTF Font
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 10000,
-            mimetype: "application/octet-stream",
-          },
-        },
-      },
-      // EOT Font
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        use: "file-loader",
-      },
-      // SVG Font
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        use: {
-          loader: "url-loader",
-          options: {
-            limit: 10000,
-            mimetype: "image/svg+xml",
-          },
-        },
-      },
-      // Common Image Formats
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg|webp)$/,
-        use: "url-loader",
-      },
-    ],
+    path: path.join(__dirname, ".."),
+    filename: "./app/main/dist/main.prod.js",
+    // https://github.com/webpack/webpack/issues/1114
+    libraryTarget: "commonjs2",
   },
 
   optimization: {
-    minimize: true,
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           compress: true,
         },
       }),
-      new OptimizeCSSAssetsPlugin(),
     ],
   },
 
   plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode:
+        process.env.OPEN_ANALYZER === "true" ? "server" : "disabled",
+      openAnalyzer: process.env.OPEN_ANALYZER === "true",
+    }),
+
     /**
      * Create global constants which can be configured at compile time.
      *
@@ -188,22 +56,18 @@ module.exports = merge.smart(baseConfig, {
      */
     new webpack.EnvironmentPlugin({
       NODE_ENV: "production",
-    }),
-
-    new webpack.DefinePlugin({
-      // https://github.com/palantir/blueprint/issues/3739.
-      "process.env.BLUEPRINT_NAMESPACE": JSON.stringify("bp5"),
-      "process.env.REACT_APP_BLUEPRINT_NAMESPACE": JSON.stringify("bp5"),
-    }),
-
-    new MiniCssExtractPlugin({
-      filename: "style.css",
-    }),
-
-    new BundleAnalyzerPlugin({
-      analyzerMode:
-        process.env.OPEN_ANALYZER === "true" ? "server" : "disabled",
-      openAnalyzer: process.env.OPEN_ANALYZER === "true",
+      DEBUG_PROD: false,
+      START_MINIMIZED: false,
     }),
   ],
+
+  /**
+   * Disables webpack processing of __dirname and __filename.
+   * If you run the bundle in node.js it falls back to these values of node.js.
+   * https://github.com/webpack/webpack/issues/2010
+   */
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
 });
